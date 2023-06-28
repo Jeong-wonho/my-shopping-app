bc19e4bed6c4d32145f6bd9d8090995521638cc6
Jeong-wonho <kallzzang@naver.com>
2023-06-29 04:32:38 +0900

    update html page

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -11,9 +12,10 @@ app.use(bodyParser.urlencoded({extended:false}));
 
 app.use('/admin', adminRoutes);
 
-app.use('/admin', shopRoutes);
+app.use(shopRoutes);
 
 app.use('/', (req, res, next) => {
-    res.status(404).send('<h1>Page not found!</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', 'error.html'));
 })
+
 app.listen(3000);

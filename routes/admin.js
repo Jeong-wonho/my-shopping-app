bca2adbc70f3ba1e58d1410fea67e00891d60b3a
Jeong-wonho <kallzzang@naver.com>
2023-07-08 20:08:29 +0900

    change code

--- routes/admin.js
+++ routes/admin.js
@@ -2,16 +2,20 @@ const path = require('path');
 
 const express = require('express');
 
-const productController = require("../controllers/products");
+const adminController = require("../controllers/admin");
 
 const router = express.Router();
 
 const products = [];
 
 // /admin/add-product => GET
-router.get('/add-product', productController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
+
+//admin/products => get
+router.get('/products', adminController.getProduct);
+
 
 // /admin/add-product => POST
-router.post('/add-product', productController.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;

9358e8e759f8090d33a96c4ea0d4a52a96c56e0f
Jeong-wonho <kallzzang@naver.com>
2023-07-11 20:54:55 +0900

    today study !

--- models/product.js
+++ models/product.js
@@ -2,10 +2,10 @@ const fs = require("fs");
 const path = require("path");
 
 const p = path.join(
-    path.dirname(require.main.filename),
-    "data",
-    "products.json"
-  );
+  path.dirname(require.main.filename),
+  "data",
+  "products.json"
+);
 
 const getProductsFromFile = (cb) => {
   fs.readFile(p, (err, fileContent) => {
@@ -18,16 +18,19 @@ const getProductsFromFile = (cb) => {
 };
 
 module.exports = class Product {
-  constructor(title) {
+  constructor(title, imageUrl, description, price) {
     this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
-    getProductsFromFile(product => {
-        product.push(this);
-        fs.writeFile(p, JSON.stringify(product), err => {
-            console.log(err);
-        })
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
     });
   }
 

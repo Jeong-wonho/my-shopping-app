ecbacc44a4213873f7fb631afe84ec99c0946739
Jeong-wonho <kallzzang@naver.com>
2023-07-12 18:04:40 +0900

    edit function add +

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,17 +1,18 @@
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render("admin/edit-product", {
+  res.render("admin/add-product", {
     pageTitle: "Add Product",
-    path: "/admin/add-product",
+    path: "/admin/edit-product",
+    editing: false,
   });
 };
 
 exports.postAddProduct = (req, res, next) => {
-    const title = req.body.title;
-    const imageUrl = req.body.imageUrl;
-    const price = req.body.price;
-    const description = req.body.description;
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
   const product = new Product(title, imageUrl, description, price);
   product.save();
   res.redirect("/");
@@ -19,23 +20,29 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
-  console.log('editMode', editMode);
-  if(!editMode) {
-    return res.redirect('/')
+  if (!editMode) {
+    return res.redirect("/");
   }
-  res.render("admin/edit-product", {
-    pageTitle: "Edit Product",
-    path: "/admin/edit-product",
-    editing: editMode
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    if (!product) {
+      return res.redirect('/');
+    }
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: editMode,
+      product: product
+    });
   });
 };
 
 exports.getProduct = (req, res, next) => {
-    Product.fetchAll((products) => {
-        res.render("admin/products", {
-          prods: products,
-          pageTitle: "Admin Products",
-          path: "/admin/products"
-        });
-      });
-}
+  Product.fetchAll((products) => {
+    res.render("admin/products", {
+      prods: products,
+      pageTitle: "Admin Products",
+      path: "/admin/products",
+    });
+  });
+};

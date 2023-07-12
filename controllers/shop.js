41d5ec4c569932681d0afd04a54313b153449003
Jeong-wonho <kallzzang@naver.com>
2023-07-12 14:21:47 +0900

    add-to-cart model add+

--- controllers/shop.js
+++ controllers/shop.js
@@ -13,9 +13,9 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId, product => {
-    console.log(product);
+    res.render('shop/product-detail', {product: product, pageTitle:product.title, path:'/products'});  
   });
-  res.redirect('/');
+  
 }
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
@@ -34,6 +34,11 @@ exports.getCart = (req, res, next) => {
   });
 };
 
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  console.log(prodId);
+  res.redirect('/cart');
+}
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
     path: '/orders',

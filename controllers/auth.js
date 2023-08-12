9d57ef191a12e2a8a6e28a2eb8d538d6da6d1a38
Jeong-wonho <kallzzang@naver.com>
2023-08-12 14:58:01 +0900

    password changes login add +

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,6 @@
+//node.js Encryption
+const crypto = require("crypto");
+
 const bcrypt = require("bcryptjs");
 
 const User = require("../models/user");
@@ -7,8 +10,10 @@ dotenv.config();
 
 // send to email library
 const SibApiV3Sdk = require("sib-api-v3-sdk");
+const user = require("../models/user");
 
-SibApiV3Sdk.ApiClient.instance.authentications["api-key"].apiKey = process.env.MAIL_API;
+SibApiV3Sdk.ApiClient.instance.authentications["api-key"].apiKey =
+  process.env.MAIL_API;
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash("error");
@@ -94,19 +99,24 @@ exports.postSignup = (req, res, next) => {
         .then((result) => {
           res.redirect("/login");
           // send to email code add +
-          new SibApiV3Sdk.TransactionalEmailsApi().sendTransacEmail({
-            subject: "Hello from the Node SDK!",
-            sender: { email: "dnjsgh1204j@gmail.com", name: "hou" },
-            replyTo: { email: "dnjsgh1204j@gmail.com", name: "hou" },
-            to: [{ name: "jeong-wonho", email: email }],
-            htmlContent:
-              "<html><body><h1>This is a transactional email {{params.bodyMessage}}</h1></body></html>",
-            params: { bodyMessage: "Made just for you!" },
-          }).then(function(data) {
-            console.log(data);
-          }, function (error) {
-            console.log(error);
-          });
+          new SibApiV3Sdk.TransactionalEmailsApi()
+            .sendTransacEmail({
+              subject: "Hello from the Node SDK!",
+              sender: { email: "dnjsgh1204j@gmail.com", name: "hou" },
+              replyTo: { email: "dnjsgh1204j@gmail.com", name: "hou" },
+              to: [{ name: "jeong-wonho", email: email }],
+              htmlContent:
+                "<html><body><h1>This is a transactional email {{params.bodyMessage}}</h1></body></html>",
+              params: { bodyMessage: "Made just for you!" },
+            })
+            .then(
+              function (data) {
+                console.log(data);
+              },
+              function (error) {
+                console.log(error);
+              }
+            );
         });
     })
     .catch((err) => {
@@ -133,4 +143,90 @@ exports.getReset = (req, res, next) => {
     pageTitle: "Reset Password",
     errorMessage: message,
   });
+};
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect("/reset");
+    }
+    const token = buffer.toString("hex");
+
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        console.log(user);
+        if (!user) {
+          req.flash("error", "No account with that email found.");
+          return res.redirect("/reset");
+        }
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600000;
+        return user.save();
+      })
+      .then((result) => {
+        res.redirect("/");
+        new SibApiV3Sdk.TransactionalEmailsApi().sendTransacEmail({
+          subject: "Password reset",
+          sender: { email: "dnjsgh1204j@gmail.com", name: "hou" },
+          replyTo: { email: "dnjsgh1204j@gmail.com", name: "hou" },
+          to: [{ name: "jeong-wonho", email: req.body.email }],
+          htmlContent: `<p>You requested a password reset</p>
+          <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password</p>`,
+          // params: { bodyMessage: "Made just for you!" },
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+};
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token;
+  User.findOne({
+    resetToken: token,
+    resetTokenExpiration: { $gt: Date.now() },
+  })
+    .then((user) => {
+      let message = req.flash("error");
+      if (message.length > 0) {
+        message = message[0];
+      } else {
+        message = null;
+      }
+      res.render("auth/new-password", {
+        path: "/new-password",
+        pageTitle: "Reset Password",
+        errorMessage: message,
+        userId: user._id.toString(),
+        passwordToken: token,
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+  let resetUser;
+
+  User.findOne({
+    resetToken: passwordToken,
+    resetTokenExpiration: { $gt: Date.now() },
+    _id: userId,
+  })
+    .then((user) => {
+      resetUser = user
+      return bcrypt.hash(newPassword, 12);
+    }).then(hashedPassword => {
+      resetUser.password = hashedPassword;
+      resetUser.resetToken = null;
+      resetUser.resetTokenExpiration = undefined;
+      return resetUser.save();
+    }).then(result => {
+      res.redirect('/login');
+    })
+    .catch((err) => console.log(err));
 };

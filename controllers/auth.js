b4dfe160adea21ea49cc6e8409d011ca9834cd03
Jeong-wonho <kallzzang@naver.com>
2023-08-30 17:23:48 +0900

    사용자 편의성 증대

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@
 const crypto = require("crypto");
 
 const bcrypt = require("bcryptjs");
+const { validationResult } = require("express-validator");
 
 const User = require("../models/user");
 const dotenv = require("dotenv");
@@ -41,12 +42,27 @@ exports.getSignup = (req, res, next) => {
     path: "/signup",
     pageTitle: "Signup",
     errorMessage: message,
+    oldInput: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    validationErrors: []
   });
 };
 
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).render("auth/login", {
+      path: "/login",
+      pageTitle: "Login",
+      errorMessage: errors.array()[0].msg,
+    });
+  }
   User.findOne({ email: email })
     .then((user) => {
       if (!user) {
@@ -79,45 +95,53 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
-  User.findOne({ email: email })
-    .then((userDoc) => {
-      if (userDoc) {
-        req.flash("error", "E-Mail exists already, please pick a diffrent one");
-        return res.redirect("/signup");
-      }
+  const errors = validationResult(req);
 
-      return bcrypt
-        .hash(password, 12)
-        .then((hashedPassword) => {
-          const user = new User({
-            email: email,
-            password: hashedPassword,
-            cart: { item: [] },
-          });
-          return user.save();
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render("auth/signup", {
+      path: "/signup",
+      pageTitle: "Signup",
+      errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email: email,
+        password: password,
+        confirmPassword: req.body.confirmPassword,
+      },
+      validationErrors: errors.array(),
+    });
+  }
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPassword) => {
+      const user = new User({
+        email: email,
+        password: hashedPassword,
+        cart: { item: [] },
+      });
+      return user.save();
+    })
+    .then((result) => {
+      res.redirect("/login");
+      // send to email code add +
+      new SibApiV3Sdk.TransactionalEmailsApi()
+        .sendTransacEmail({
+          subject: "Hello from the Node SDK!",
+          sender: { email: "dnjsgh1204j@gmail.com", name: "hou" },
+          replyTo: { email: "dnjsgh1204j@gmail.com", name: "hou" },
+          to: [{ name: "jeong-wonho", email: email }],
+          htmlContent:
+            "<html><body><h1>This is a transactional email {{params.bodyMessage}}</h1></body></html>",
+          params: { bodyMessage: "Made just for you!" },
         })
-        .then((result) => {
-          res.redirect("/login");
-          // send to email code add +
-          new SibApiV3Sdk.TransactionalEmailsApi()
-            .sendTransacEmail({
-              subject: "Hello from the Node SDK!",
-              sender: { email: "dnjsgh1204j@gmail.com", name: "hou" },
-              replyTo: { email: "dnjsgh1204j@gmail.com", name: "hou" },
-              to: [{ name: "jeong-wonho", email: email }],
-              htmlContent:
-                "<html><body><h1>This is a transactional email {{params.bodyMessage}}</h1></body></html>",
-              params: { bodyMessage: "Made just for you!" },
-            })
-            .then(
-              function (data) {
-                console.log(data);
-              },
-              function (error) {
-                console.log(error);
-              }
-            );
-        });
+        .then(
+          function (data) {
+            console.log(data);
+          },
+          function (error) {
+            console.log(error);
+          }
+        );
     })
     .catch((err) => {
       console.log(err);
@@ -151,15 +175,16 @@ exports.postReset = (req, res, next) => {
       console.log(err);
       return res.redirect("/reset");
     }
+
     const token = buffer.toString("hex");
 
     User.findOne({ email: req.body.email })
       .then((user) => {
-        console.log(user);
         if (!user) {
           req.flash("error", "No account with that email found.");
           return res.redirect("/reset");
         }
+
         user.resetToken = token;
         user.resetTokenExpiration = Date.now() + 3600000;
         return user.save();
@@ -218,15 +243,17 @@ exports.postNewPassword = (req, res, next) => {
     _id: userId,
   })
     .then((user) => {
-      resetUser = user
+      resetUser = user;
       return bcrypt.hash(newPassword, 12);
-    }).then(hashedPassword => {
+    })
+    .then((hashedPassword) => {
       resetUser.password = hashedPassword;
       resetUser.resetToken = null;
       resetUser.resetTokenExpiration = undefined;
       return resetUser.save();
-    }).then(result => {
-      res.redirect('/login');
+    })
+    .then((result) => {
+      res.redirect("/login");
     })
     .catch((err) => console.log(err));
 };
